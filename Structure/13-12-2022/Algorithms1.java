7fb613ecffbd15c42efbfb6b0bbc8a33c1c61929
Jayr Jakkrit <73011056+jayrboy@users.noreply.github.com>
2022-12-19 23:42:28 +0700

    Update Algorithms1.java

--- Structure/13-12-2022/Algorithms1.java
+++ Structure/13-12-2022/Algorithms1.java
@@ -1,1 +1,13 @@
-
+public class Algorithms1 {
+    public static void main (String[]args) {
+        
+        boolean b1 = false;
+        int num = 12;
+        double real = 12.38;
+        
+        System.out.println("b1 = "+ b1);
+        // printf("b1 = %lf \n", num );     print in C 
+        System.out.println(num);
+        System.out.println(real);
+    }
+}
