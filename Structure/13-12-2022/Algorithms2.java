359f50cda7ef2a77ca82363b140e3f76f01fc6a4
Jayr Jakkrit <73011056+jayrboy@users.noreply.github.com>
2022-12-19 23:43:15 +0700

    Add files via upload

---
+++ Structure/13-12-2022/Algorithms2.java
@@ -0,0 +1,17 @@
+public class Algorithms2 {
+    public static void main (String [] args){
+        System.out.println(1+2+3);
+        int a = 1 + 2 + 3;
+        System.out.println("a = "+a);
+        a = a + 1;
+        System.out.println("a = "+a);
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        System.out.println(7/3);
+        System.out.println(7.0/3);
+        System.out.println(7/3.0);
+        System.out.println(7.0/3.0);
+        
+        
+    }
+    
+}
---
+++ Structure/13-12-2022/Algorithms3.java
@@ -0,0 +1,17 @@
+public class Algorithms3 {
+    public static void main(String[] args) {    //abrv. => psvm + tab
+        System.out.println(7>8);     //abrv. => sout + tap
+        System.out.println(11>8);
+        System.out.println(11 != 8);
+        System.out.println(11 == 8);
+        
+        boolean b = 35 > 34;
+        if (b){
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fall");
+        }
+        System.out.println("b = "+ b);
+            
+    }
+}
